2288689518ee208212001b619fc4523cdc61fa47
Shubh9907 <shubhamv60047@gmail.com>
2021-11-16 22:02:40 +0530

    Added code:- add weekday and weekend rates for each hotel

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
+import java.text.ParseException;
+
 public class HotelReservationSystem {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException, HotelReservationExceptions {
         System.out.println("Welcome to Hotel Reservation System");
+        HotelReservationSystemOperations operations = new HotelReservationSystemOperations();
+
+        operations.addHotels();
+        operations.findCheapestHotel("rewarded", "101/12/2022", "10/12/2023");
     }
 }
--- src/main/java/com/bridgelabz/HotelReservationSystemOperations.java
+++ src/main/java/com/bridgelabz/HotelReservationSystemOperations.java
@@ -1,26 +1,86 @@
 package com.bridgelabz;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class HotelReservationSystemOperations {
-    ArrayList<Hotels> list = new ArrayList<>();
-    Scanner scan = new Scanner(System.in);
+    ArrayList<Hotels> hotelList = new ArrayList<>();
+    static Scanner scan = new Scanner(System.in);
 
     /*
     Method to add details of hotels
      */
     public void addHotels() {
-        Hotels hotels = new Hotels();
+        hotelList.add(new Hotels("Lakewood", 110, 90, 80, 80, 3));
+        hotelList.add(new Hotels("Bridgewood", 160, 60, 110, 50, 4));
+        hotelList.add(new Hotels("Ridgewood", 220, 150, 100, 40, 5));
+    }
 
-        System.out.println("Enter Hotel name");
-        String hotelName = scan.nextLine();
-        hotels.setHotelName(hotelName);
+    /*
+    Method to find the cheapest hotel in a given date range
+     */
+    public String findCheapestHotel(String customerType, String checkInDate, String checkOutDate) throws ParseException , HotelReservationExceptions , NullPointerException {
+        final String DATE_PATTERN = "^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$";
+        Date inDate = null;
+        Date outDate = null;
+        int bestRates = 0;
+        boolean check = true;
 
-        System.out.println("Enter Rates for Regular Customer");
-        int rates = scan.nextInt();
-        hotels.setRegularRates(rates);
+        Hotels cheapest = hotelList.get(0);
 
-        this.list.add(hotels);
+        try {
+            if (Pattern.matches(DATE_PATTERN, checkInDate) && Pattern.matches(DATE_PATTERN, checkOutDate)) {
+                inDate = new SimpleDateFormat("dd/MM/yy").parse(checkInDate);
+                outDate = new SimpleDateFormat("dd/MM/yy").parse(checkOutDate);
+            }
+            if (customerType.equalsIgnoreCase("REGULAR")) {
+                if (inDate.getDay() == 6 || inDate.getDay() == 7) {
+                    for (Hotels hotels : hotelList) {
+                        if (cheapest.getRegularWeekendRates() > hotels.getRegularWeekendRates()) {
+                            cheapest = hotels;
+                            bestRates = cheapest.getRegularWeekendRates();
+                        }
+                    }
+                } else {
+                    for (Hotels hotels : hotelList) {
+                        if (cheapest.getRegularWeekdaysRates() > hotels.getRegularWeekdaysRates()) {
+                            cheapest = hotels;
+                            bestRates = cheapest.getRegularWeekdaysRates();
+                        }
+                    }
+                }
+            } else if (customerType.equalsIgnoreCase("Rewarded")) {
+                if (inDate.getDay() == 6 || inDate.getDay() == 7) {
+                    for (Hotels hotels : hotelList) {
+                        if (cheapest.getRewardedWeekendRates() > hotels.getRewardedWeekendRates()) {
+                            cheapest = hotels;
+                            bestRates = cheapest.getRewardedWeekendRates();
+                        }
+                    }
+                } else {
+                    for (Hotels hotels : hotelList) {
+                        if (cheapest.getRewardedWeekdaysRates() > hotels.getRewardedWeekdaysRates()) {
+                            cheapest = hotels;
+                            bestRates = cheapest.getRewardedWeekdaysRates();
+                        }
+                    }
+                }
+            } else {
+                System.out.println("Please Enter a valid Customer Type, Enter Again");
+                check = false;
+                findCheapestHotel(customerType, checkInDate, checkOutDate);
+            }
+            if (check == true) {
+                System.out.println("The Cheapest hotel is:-\n" + cheapest.toString());
+                System.out.println("Best Rates for you is- " + bestRates);
+            }
+        }catch (NullPointerException e) {
+        System.out.println("You Entered an invalid date");
+    }
+        return cheapest.getHotelName();
     }
 }
