3bbea7093250b6bdb41eafd728efc7a6f619e964
Shubh9907 <shubhamv60047@gmail.com>
2021-11-21 17:58:03 +0530

    Added code:- Code added to find cheapest best rated hotel for regular customers using java streams

--- src/test/java/com/bridgelabz/HotelTest.java
+++ src/test/java/com/bridgelabz/HotelTest.java
@@ -1,16 +1,19 @@
 package com.bridgelabz;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 
 class HotelTest {
 
+    HotelReservationSystemOperations obj = new HotelReservationSystemOperations();
+
     @Test
     void givenFirstHotelName_WhenAddedToList_ShouldHaveTheDetails() {
         boolean result = false;
-        HotelReservationSystemOperations obj = new HotelReservationSystemOperations();
         obj.addHotels();
         int listSize = obj.hotelList.size();
         if (listSize > 0) {
@@ -21,9 +24,16 @@ class HotelTest {
 
     @Test
     void givenCustomerType_WhenCheck_ShouldReturnCheapestHotel() throws ParseException, HotelReservationExceptions {
-        HotelReservationSystemOperations obj = new HotelReservationSystemOperations();
         obj.addHotels();
         String result = obj.findCheapestHotel("REWARDED", "27/11/2021", "28/11/2021");
-        Assertions.assertEquals("Ridgewood",result);
+        Assertions.assertEquals("Ridgewood", result);
+    }
+
+    @Test
+    void givenCustomerType_WhenCheck_ShouldReturnCheapestBestRatedHotel() throws HotelReservationExceptions, ParseException {
+        obj.addHotels();
+        obj.findCheapestHotel("REWARDED", "27/11/2021", "28/11/2021");
+        String result = obj.bestRatedHotel();
+        Assertions.assertEquals("Ridgewood", result);
     }
 }
